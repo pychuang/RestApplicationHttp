4b2c5ac5c2d72309e23e3e9eec87faeaff58b800
Po-Yu Chuang <pochuang@microsoft.com>
2020-07-15 16:51:12 -0700

    Add project files.

---
+++ RestApplicationHttp/Controllers/DataController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApplicationHttp.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet("random/{dataSize}")]
+        public IActionResult Get([FromRoute] long dataSize)
+        {
+            Console.WriteLine($"{this.Request.Method} {this.Request.GetDisplayUrl()}");
+            this.PrintHeaders();
+
+            var stream = new RandomReadOnlyStream(dataSize);
+            return this.Ok(stream);
+        }
+
+        [HttpPost("random/{dataSize}")]
+        public async Task<IActionResult> PostAsync([FromRoute] long dataSize)
+        {
+            Console.WriteLine($"{this.Request.Method} {this.Request.GetDisplayUrl()}");
+            this.PrintHeaders();
+
+            long totalBytesRead = await this.ReadStreamAsync(this.Request.Body);
+            Console.WriteLine($"totalBytesRead {totalBytesRead}");
+            var stream = new RandomReadOnlyStream(dataSize);
+            return this.Ok(stream);
+        }
+
+        [HttpPost("copy")]
+        public IActionResult Post()
+        {
+            Console.WriteLine($"{this.Request.Method} {this.Request.GetDisplayUrl()}");
+            this.PrintHeaders();
+
+            return this.Ok(this.Request.Body);
+        }
+
+        private async Task<long> ReadStreamAsync(Stream stream)
+        {
+            long totalBytesRead = 0;
+            byte[] buffer = new byte[1024];
+            bool skipPrinting = false;
+            while (true)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead <= 0)
+                {
+                    break;
+                }
+
+                totalBytesRead += bytesRead;
+                Console.WriteLine($"totalBytesRead {totalBytesRead}");
+
+                string s = Encoding.Default.GetString(buffer, 0, bytesRead);
+                if (totalBytesRead <= 1024)
+                {
+                    Console.Write(s);
+                }
+                else if (!skipPrinting)
+                {
+                    Console.WriteLine("...");
+                    skipPrinting = true;
+                }
+            }
+
+            Console.WriteLine($"Total bytes: {totalBytesRead}");
+            Console.WriteLine();
+            return totalBytesRead;
+        }
+
+        private void PrintHeaders()
+        {
+            foreach (var header in this.Request.Headers)
+            {
+                Console.WriteLine($"{header.Key}={header.Value}");
+            }
+        }
+    }
+}
---
+++ RestApplicationHttp/RandomReadOnlyStream.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApplicationHttp
+{
+    public class RandomReadOnlyStream : Stream
+    {
+        private const int lineWidth = 64;
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length { get; }
+
+        public override long Position { get; set; } = 0;
+
+        private static Random random = new Random();
+
+        public RandomReadOnlyStream(long length)
+        {
+            this.Length = length;
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            //const string chars = "0123456789";
+
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                if (this.Position >= this.Length)
+                {
+                    break;
+                }
+
+                char c;
+                if (this.Position % lineWidth == lineWidth - 1)
+                {
+                    c = '\n';
+                }
+                else
+                {
+                    c = chars[random.Next(chars.Length)];
+                    //c = chars[(int)(this.Position % 10)];
+                }
+
+                buffer[offset + i] = Convert.ToByte(c);
+                this.Position++;
+
+                if (this.Position != 0 && this.Position % 1000 == 0)
+                {
+                    Console.WriteLine($"position {this.Position}");
+                }
+            }
+
+            return i;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
